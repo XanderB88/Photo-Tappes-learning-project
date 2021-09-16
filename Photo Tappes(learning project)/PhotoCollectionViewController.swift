fde64d6d4bacdeacf0464ff9fb6cbfc0d7c66159
XanderB88 <alexxanderbykov@gmail.com>
2021-09-16 06:03:52 +0400

    Configure a Collection View Cell and add photos to it.

--- Photo Tappes(learning project)/PhotoCollectionViewController.swift
+++ Photo Tappes(learning project)/PhotoCollectionViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class PhotoCollectionViewController: UICollectionViewController {
-
+  
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+    
     let itemsPerRow: CGFloat = 2
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,14 +39,17 @@ class PhotoCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCollectionViewCell
+        
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
         
         // Configure the cell
-        cell.backgroundColor = .black
+        cell.photoImageView.image = image
         return cell
     }
 }
