b1c597b4396904202cf329e03715a0857b1a166a
XanderB88 <alexxanderbykov@gmail.com>
2021-09-17 03:52:31 +0400

    Add a detail screen with photo and connect it to main screen.

--- Photo Tappes(learning project)/PhotoCollectionViewController.swift
+++ Photo Tappes(learning project)/PhotoCollectionViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PhotoCollectionViewController: UICollectionViewController {
-  
+    
     let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
     
     let itemsPerRow: CGFloat = 2
@@ -17,31 +17,29 @@ class PhotoCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "detailPhoto" {
+            let detailPhotoVC = segue.destination as! DetailPhotoViewController
+            let cell = sender as! PhotoCollectionViewCell
+            detailPhotoVC.image = cell.photoImageView.image
+        }
     }
-    */
-
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return photos.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCollectionViewCell
         
